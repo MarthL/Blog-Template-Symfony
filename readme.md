e1cffa6cb9e0f910f896f0fa410d0de3facc6ae2
martin <martin.aubertin@90tech.fr>
2022-01-22 12:07:04 +0100

    blog ok. faire template

---
+++ readme.md
@@ -0,0 +1,13 @@
+** TO-DO List 
+
+Multilangue
+Navbar 
+Page des articles dans blog
+
+
+** Blog 
+
+créer une page d'administration pour gérer le blog avec différents crud 
+ajouter le slug, lien pour chaque article 
+probleme pour afficher article selon id -> voir dans repository ? 
+
---
+++ src/DataFixtures/ArticleFixtures.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Article;
+use Symfony\Component\String\Slugger\AsciiSlugger;
+use Faker\Factory;
+
+class ArticleFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // On boucle 20x pour créer 20 colonnes 
+        for($i = 0; $i < 20; $i++)
+        {
+        // Création des articles
+        $faker = Factory::create('fr_FR');
+        $slugger = new AsciiSlugger();
+
+            for($i = 0; $i < 20; $i++)
+            {
+                // attribution des setters pour chaque objet 
+                $title = $faker->sentence(5, true);
+                $article = (new Article())
+                ->setTitle($title)
+                ->setContent($faker->text(100))
+                ->setDate($faker->dateTime())
+                ->setImage("https://picsum.photos/200/300?random=" . $i)
+                ->setUrl("/".$i)
+                ->setSlug(strtolower($slugger->slug($title)));
+                
+                // On persiste l'objet
+                $manager->persist($article);
+            }
+                
+            // On déclenche l'enregistrement en BDD
+            $manager->flush();
+        }
+    }
+}
