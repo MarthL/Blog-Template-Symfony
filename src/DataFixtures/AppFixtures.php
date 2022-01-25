1ea66362d715244600ffbad815efa995683721ab
martin <martin.aubertin@90tech.fr>
2022-01-25 14:02:30 +0100

    ajout de la sécurité. redirection automatique sur login. reste à rattacher user pour le traitement du form

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,17 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // create one user for example
+        $user = new User();
+        $user->setUsername('johnDoe');
+        $user->setPassword('password');
+        $manager->persist($user);
 
         $manager->flush();
     }
