c0a4828753585575360fdd655bf243c8dd597af8
martin <martin.aubertin@90tech.fr>
2022-02-05 15:27:37 +0100

    Event sur navbar, à voir avec Baptiste

--- public/assets/app.js
+++ public/assets/app.js
@@ -5,6 +5,8 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+import { gsap } from 'gsap/dist/gsap'
+
 // any CSS you import will output into a single css file (app.css in this case)
 import '../../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
@@ -12,3 +14,24 @@ import './styles/app.scss';
 import './styles/parts/footer.scss';
 import './styles/parts/header.scss';
 import './styles/home.scss';
+
+
+
+gsap.from("#text__hero", {
+    opacity: 0,
+    y: -50,
+    duration: 1
+});
+
+let navbar = document.getElementsByClassName('navbar');
+
+
+
+// tout marche, le problème c'est bootstrap, il faut override le css
+window.addEventListener("scroll", function () { 
+    if(window.scrollY > 0 ) {
+        navbar[0].classList.remove('bg-light');
+    } else { 
+        navbar[0].classList.add('bg-light');
+    }
+})
